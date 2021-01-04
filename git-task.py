ca02aa876ff5a7e6fadbd9697fe2fa8cf7e393ea
Albert Pulinski <albertpulinski@gmail.com>
2021-01-04 13:40:34 +0100

    Add git-task.py

--- git-task.py
+++ git-task.py
@@ -1,12 +1,1 @@
-print("Lista zakupów")
-
-zakupy_dict = {
-    "piekarnia": ["chleb","pączek","bułki"],
-    "warzywniak": ["marchew","seler","rukola"]
-}
-
-for sklep, produkty in zakupy_dict.items():
-    print(sklep, ":", produkty)
-
-    print(f"Idę do ",(sklep.capitalize()),"kupuję tu następujące rzeczy", (produkty))
-    print(f"W sumie kupuję", len(produkty), "produkty.")
+print("Zmiana nr 1")
